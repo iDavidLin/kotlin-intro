5aa3618a90d65341453da9e464232d4f296b668e
David <linyongxi7@gmail.com>
2021-07-02 15:48:37 +1000

    Update TailRecursion.kt

--- src/main/kotlin/com/advanced/functions/TailRecursion.kt
+++ src/main/kotlin/com/advanced/functions/TailRecursion.kt
@@ -11,16 +11,3 @@ fun factorial(number: Int): Int {
 fun main() {
     println(factorial(1000000))
 }
-
-/**
- * Tail Call optimization is a technique that compilers can perform to essentially convert a function call to a loop. Kotlin allows us to denote certain tail recursive functions to be optimized.
- * Kotlin is not doing this for default
- * */
-
-/**
- * What is tail recursion?
- * How kotlin resolve this question?
- * */
-
-// return a GOTO
-// replace a call with a loop?
