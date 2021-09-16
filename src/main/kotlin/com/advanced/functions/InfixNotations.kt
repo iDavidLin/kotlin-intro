881ea8ce282b1f7d5a9797a4ec82c0e060abec4b
david.lin <linyongxi7@gmail.com>
2021-09-17 08:52:25 +1000

    infix notations

---
+++ src/main/kotlin/com/advanced/functions/InfixNotations.kt
@@ -0,0 +1,5 @@
+package com.advanced.functions
+
+//infix fun String.shouldBeEqualTo(value: String) = this == value
+
+data class User(val name: String, val age: Int)
---
+++ src/test/kotlin/com/advanced/functions/InfixNotationsTest.kt
@@ -0,0 +1,23 @@
+package com.advanced.functions
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class InfixNotationsTest {
+    /*
+    @Test
+    fun `infix for extension function`() {
+        Assertions.assertTrue("string 1" shouldBeEqualTo "string 1")
+    }
+     */
+
+    /*
+    @Test
+    fun `infix of member function`() {
+        val Bob = User("Bob", 25)
+        val Jim = User("Jim", 25)
+
+        Assertions.assertTrue( Bob hasSameAge Jim )
+    }
+     */
+}
