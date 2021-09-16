e6ca651fbce094f02592b3c1cd67c4a0a23e507b
david.lin <linyongxi7@gmail.com>
2021-09-17 08:53:03 +1000

    group impl

---
+++ src/main/kotlin/com/advanced/5 - generics/GroupImpl.kt
@@ -0,0 +1,20 @@
+package com.advanced.`5 - generics`
+
+class GroupImpl<Mammal> : Group<com.advanced.`5 - generics`.Mammal> {
+    override fun fetch(): com.advanced.`5 - generics`.Mammal {
+        TODO("Not yet implemented")
+    }
+
+    override fun insert(item: com.advanced.`5 - generics`.Mammal) {
+        TODO("Not yet implemented")
+    }
+}
+class GroupImplx<Sloths> : Group<com.advanced.`5 - generics`.Sloths> {
+    override fun fetch(): com.advanced.`5 - generics`.Sloths {
+        TODO("Not yet implemented")
+    }
+
+    override fun insert(item: com.advanced.`5 - generics`.Sloths) {
+        TODO("Not yet implemented")
+    }
+}
